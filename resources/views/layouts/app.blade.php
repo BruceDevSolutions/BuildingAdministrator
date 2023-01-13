f7b4d6300766431f4271aba27345813784388cf7
Bruce Oliva <brucedevsolutions@gmail.com>
2023-01-13 17:04:49 -0400

    Inicio del proyecto, ya está confrigurada la plantilla y el modo oscuro. Funcionando con Alpine 3, estoy a punto de comenzar a crear algunas de las tablas y modelos

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html x-data="data()" lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+
+	<title>Windmill Dashboard</title>
+
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+	<link rel="stylesheet" href="{{ asset('/assets/css/tailwind.output.css') }}" />
+
+	<script src=" {{ asset('/assets/js/init-alpine.js') }}"></script>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Styles -->
+    @livewireStyles
+</head>
+
+<body>
+	<div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+        
+        <x-app.sidebar />
+        
+        <div class="flex flex-col flex-1 w-full">
+
+            <x-app.header />
+
+			<main class="h-full overflow-y-auto">
+                <div class="container px-6 grid mx-auto">
+                    {{ $slot }}
+                </div>
+			</main>
+		</div>
+	</div>
+
+{{-- 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+    <script src="./assets/js/charts-lines.js" defer></script>
+	<script src="./assets/js/charts-pie.js" defer></script>  --}}
+
+    @stack('modals')
+
+    @livewireScripts
+</body>
+
+</html>
